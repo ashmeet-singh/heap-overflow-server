6c9dceea9122949350fb8c3c9a229ecf5f30ce91
ashmeet-singh <76838543+ashmeet-singh@users.noreply.github.com>
2022-02-19 21:59:47 +0530

    Receiving name and password for signup

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	// "io"
+	"net/http"
+	// "net/url"
+	"context"
+	"os"
+	// "path"
+	// "strconv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+const uri = "mongodb://localhost:27017"
+
+type JSON_T1 struct {
+	Name     string `json:"name"`
+	Password string `json:"password"`
+}
+
+func main() {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	// Ping the primary
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully connected and pinged.")
+
+	http.Handle("/", http.FileServer(http.Dir(os.Getenv("HO_DIR_WEBSITE"))))
+
+	http.HandleFunc("/api/signup", func(w http.ResponseWriter, r *http.Request) {
+		b := make([]byte, 1000)
+		l, _ := r.Body.Read(b)
+		m := JSON_T1{}
+		json.Unmarshal(b[:l], &m)
+		fmt.Println(string(b))
+		fmt.Println(m.Name)
+		fmt.Println(m.Password)
+	})
+
+	err1 := http.ListenAndServe(":8080", nil)
+	if err1 != nil {
+		fmt.Println("Unable to listen and serve")
+	}
+}
